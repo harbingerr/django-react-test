fc4e823cdb5ce1aa76d66e762fc1d27c33028dad
harbingerr <milan.bohna@gmail.com>
2024-03-01 20:17:17 +0100

    fix default

--- api/models.py
+++ api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Player(models.Model):
-    name = models.CharField(_default="", max_length=50)
+    name = models.CharField(default="", max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
