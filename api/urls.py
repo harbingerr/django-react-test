57d91a6f9b3f3ef89af8b6bd1b7d1f5210df8958
harbingerr <milan.bohna@gmail.com>
2024-03-01 20:55:38 +0100

    Get-post player view

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import PlayerView, PlayerPostView, homePage
 
 urlpatterns = [
     path('', homePage),
-    path('api/player/get', PlayerView.as_view())
-    path('api/player/post', PlayerPostView.as_view())
+    path('api/player/get', PlayerView.as_view()),
+    path('api/player/post', PlayerPostView.as_view()),
 ]
