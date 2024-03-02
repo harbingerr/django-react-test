be588c442e24f5e6f63cee819ad9cdc0531f0ede
harbingerr <milan.bohna@gmail.com>
2024-03-02 09:06:26 +0100

    Trying to connect fe

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import PlayerGetView, PlayerPostView, homePage
 
 urlpatterns = [
-    path('', homePage),
-    path('api/player/get', PlayerGetView.as_view()),
-    path('api/player/post', PlayerPostView.as_view()),
+    path('player/get', PlayerGetView.as_view()),
+    path('player/post', PlayerPostView.as_view()),
 ]
